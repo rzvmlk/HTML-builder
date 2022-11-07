ca1372b7de537ffc92ea5e8b670a78e4a556814b
Nadia Halkina <nadia.esperanza.2000@gmail.com>
2022-11-07 20:01:53 +0100

    feat: task 1

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 
-
 const absPath = path.join( __dirname, './text.txt');
 const input = fs.createReadStream(absPath);
 input.pipe(process.stdout);
